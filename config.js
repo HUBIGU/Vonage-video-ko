4ac20a36f9a3d49b1bed21f310a6529ec7bc18b2
HUBIGU <169113196+HUBIGU@users.noreply.github.com>
2024-11-06 10:56:45 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+
+// 设置 VCR 服务器的 URL，指向 /session 路径，返回 Vonage 会话信息。
+const SAMPLE_SERVER_BASE_URL = 'https://neru-48221537-vonage-video-backend-server-dev.apse1.runtime.vonage.cloud';
+
+// 清空这些变量，因为我们将在前端通过后端动态获取它们。
+const APPLICATION_ID = '';
+const SESSION_ID = '';
+const TOKEN = '';
